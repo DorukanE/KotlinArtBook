ce0425620c0a7e13ed47d1f4eabe1c8294800ee9
DorukanE <dorukaneskiceri@gmail.com>
2020-02-17 22:03:11 +0300

    Initial commit

---
+++ app/src/main/java/com/dorukaneskiceri/kotlinartbook/ArtActivity.kt
@@ -0,0 +1,12 @@
+package com.dorukaneskiceri.kotlinartbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art)
+    }
+}
