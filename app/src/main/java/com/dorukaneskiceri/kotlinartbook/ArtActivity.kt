70286517b4c395da6c1f9781800f1ddb89bbd2ea
DorukanE <dorukaneskiceri@gmail.com>
2020-02-17 23:33:43 +0300

    added user permissons

--- app/src/main/java/com/dorukaneskiceri/kotlinartbook/ArtActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/kotlinartbook/ArtActivity.kt
@@ -1,8 +1,14 @@
 package com.dorukaneskiceri.kotlinartbook
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class ArtActivity : AppCompatActivity() {
 
@@ -17,5 +23,28 @@ class ArtActivity : AppCompatActivity() {
 
     fun selectImage(view: View){
 
+        //Request Permission
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        }else{
+            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intentToGallery,2)
+        }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == 1){
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intentToGallery, 2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    
 }
