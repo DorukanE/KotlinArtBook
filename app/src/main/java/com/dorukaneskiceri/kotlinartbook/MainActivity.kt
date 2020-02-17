f6313c35932857c31f9a12435a893ee7f595858f
DorukanE <dorukaneskiceri@gmail.com>
2020-02-17 22:44:42 +0300

    added menu and designs

--- app/src/main/java/com/dorukaneskiceri/kotlinartbook/MainActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/kotlinartbook/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.dorukaneskiceri.kotlinartbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        //Menu-Inflater
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.add_art,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add_art_item){
+            val intent = Intent(this,ArtActivity::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
