84aa9772c71470783facf3c2971bf1708cff0594
DorukanE <dorukaneskiceri@gmail.com>
2020-02-17 22:23:42 +0300

    added design components

--- app/src/main/java/com/dorukaneskiceri/kotlinartbook/ArtActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/kotlinartbook/ArtActivity.kt
@@ -2,11 +2,20 @@ package com.dorukaneskiceri.kotlinartbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ArtActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_art)
+    }
+
+    fun save(view: View){
+
+    }
+
+    fun selectImage(view: View){
+
     }
 }
